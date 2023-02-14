b05ead9491a23c16259110dfc62310a801c86b00
alexxx2023 <125368301+alexxx2023@users.noreply.github.com>
2023-02-14 09:55:54 -0300

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,64 @@
+javascript.js
+/*@@ -0,0 +1,65 @@ */
+function enAmarillo1() {
+Element = document.getElementById("pers");
+Element.src="img/personal";
+}
+function enNegro1() {
+    Element = document.getElementById("pers");
+    Element.src="https://e7.pngegg.com/pngimages/168/294/png-clipart-buttons-buttons.png";
+}
+
+function enAmarillo2() {
+    Element = document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element = document.getElementById("labo");
+    Element.src="https://e7.pngegg.com/pngimages/168/294/png-clipart-buttons-buttons.png";
+}
+
+function enAmarillo3() {
+    Element = document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element = document.getElementById("habi");
+    Element.src="https://e7.pngegg.com/pngimages/168/294/png-clipart-buttons-buttons.png";
+}
+
+function enAmarillo4() {
+    Element = document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element = document.getElementById("estu");
+    Element.src="https://e7.pngegg.com/pngimages/168/294/png-clipart-buttons-buttons.png";
+}
+
+function enAmarillo5() {
+    Element = document.getElementById("be");
+    Element.src="";
+}
+function enNegro5() {
+    Element = document.getElementById("be");
+    Element.src="";
+}
+
+function enAmarillo6() {
+    Element = document.getElementById("gi");
+    Element.src="";
+}
+function enNegro6() {
+    Element = document.getElementById("gi");
+    Element.src="";
+}
+
+function enAmarillo7() {
+    Element = document.getElementById("li");
+    Element.src="";
+}
+function enNegro7() {
+    Element = document.getElementById("li");
+    Element.src="";
+} 
